fc354658d971ee8c42b3839fe8ad7335e6f3451d
Rghhemendra Gour <hemendramu76@gmail.com>
2024-09-30 16:04:58 +0530

    program2

--- program2.py
+++ program2.py
@@ -4,7 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
-
-
+        # Roman numerals and their corresponding integer values
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        # Iterate through the string
+        for i in range(n):
+            # If current character is less than the next character, subtract its value
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add its value
+                total += roman_map[s[i]]
+        
+        return total
 
